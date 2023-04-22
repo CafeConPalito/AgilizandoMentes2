74f98f267d900c31850f7c846d945cdaa6ee9a66
Daniel-Espinosa <anareldis@gmail.com>
2023-04-22 19:04:53 +0200

    Inserts en BBDD y MetodosCalculo Java

--- AgilizandoMentesII/src/Main/Principal.java
+++ AgilizandoMentesII/src/Main/Principal.java
@@ -5,11 +5,13 @@
 package Main;
 
 import BBDD.ConexionBBDD;
+import BBDD.MetodosCalculo;
 import BBDD.MetodosUsuario;
 import Calculadora.Calculadora;
 import Login.Login;
 import Usuario.Usuario;
 import java.sql.*;
+import java.time.LocalDate;
 
 /**
  *
@@ -33,6 +35,10 @@ public class Principal {
         //Usuario user = new Usuario();
         
         String usuario = "Cbarrios";
+        //String usuario = "Despinosa";
+        //String usuario = "Rgutierrez";
+        //String usuario = "Adiez";
+        
         /*
         String contrasena = "12345";
         String DNI = "12345678T";
@@ -48,12 +54,30 @@ public class Principal {
         System.out.println("Es Profesor " + MetodosUsuario.comprobarEsProfesor(con, nombreUsuario));    
         */
         
+        
         Usuario.setUsuario(usuario);
         MetodosUsuario.generarUsuario(con);
-        System.out.println(Usuario.getAlias() + Usuario.getApellido1() + Usuario.getApellido2() + Usuario.getNombre());
+        //System.out.println(Usuario.getAlias() + Usuario.getApellido1() + Usuario.getApellido2() + Usuario.getNombre());
         
-        Calculadora calcu = new Calculadora();
-        calcu.setVisible(true);
+        //Calculadora calcu = new Calculadora();
+        //calcu.setVisible(true);
+        
+        //devuelve la fecha actual
+        //System.out.println(Date.valueOf(LocalDate.now()));
+        
+        System.out.println("Insertar Datos Partida Calculo25");
+        //MetodosCalculo.insertResultado(con,28, 14);
+        System.out.println("fin insertar datos");
+        System.out.println("Mejores 5 partidas del jugador " + Usuario.getUsuario());
+        MetodosCalculo.selectJugadorMejoresPartidas(con);
+        System.out.println("Fin de Mejores 5 patidas del Jugador ");
+        System.out.println("Mostrando las mejores 5 partidas de todos los jugadores de ese nivel");
+        MetodosCalculo.selectClasificacion(con);
+        System.out.println("fin de mostrar las mejores partidas de los jugadores de ese nivel");
+        System.out.println("Mostrando las ultimas 5 Partidas del Jugador");
+        MetodosCalculo.selectJugadorUltimasPartidas(con);
+        System.out.println("fin mostrando ultimas partidas");
+        
         
         conBD.desconectar();
     }
